6f2766d5bcebe24201395adcc6804459feb13e3c
Gerard Hynes <gkhynes@gmail.com>
2019-11-09 11:21:20 +0000

    Add Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Calculator from "../Calculator/Calculator";
+import Footer from "../Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      Built by
+      <a href="https://github.com/GK-Hynes"> Gerard Hynes</a>
+    </footer>
+  );
+}
